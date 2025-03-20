7209a97038a7fcf43abcf089c19dab84703fd17c
fromjkeee16 <fromjkeee16@gmail.com>
2025-03-20 23:20:08 +0200

    fc -> class

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-export const App: React.FC = () => {
-  const [key, setKey] = useState<null | string>(null);
-
-  useEffect(() => {
-    const handleKeyPress = (event: KeyboardEvent) => {
-      setKey(event.key);
-    };
-
-    document.addEventListener('keyup', handleKeyPress);
-
-    return () => {
-      document.removeEventListener('keyup', handleKeyPress);
-    };
-  }, []);
-
-  return (
-    <div className="App">
-      <p className="App__message">
-        {key ? `The last pressed key is [${key}]` : 'Nothing was pressed yet'}
-      </p>
-    </div>
-  );
+type State = {
+  key: string | null;
 };
+
+export class App extends React.Component {
+  state: Readonly<State> = {
+    key: null,
+  };
+
+  handleKeyPress = (event: KeyboardEvent) => {
+    this.setState({ key: event.key });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyPress);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyPress);
+  }
+
+  render() {
+    const { key } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {key ? `The last pressed key is [${key}]` : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
